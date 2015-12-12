f7d57d29cfb2ccf4b9286ee7ed379349bdd6e81a
sgomez <santii810@hotmail.com>
2015-12-12 16:47:57 +0100

    Insertada la api de facebook y el inicio se sesion

--- app/src/main/java/sgomez/ejemplos/proyectopmul02/MainActivity.java
+++ app/src/main/java/sgomez/ejemplos/proyectopmul02/MainActivity.java
@@ -4,8 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.parse.Parse;
+
 
 import sgomez.ejemplos.proyectopmul02.model.CamareroRepository;
 import sgomez.ejemplos.proyectopmul02.model.CocinaRepository;
@@ -22,17 +30,53 @@ public class MainActivity extends AppCompatActivity {
     private static LocalRepository localRepository;
     private static ExtraRepository extraRepository;
 
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Iniciar Parse
         Parse.initialize(this, getResources().getString(R.string.parseId), getResources().getString(R.string.parseKey));
         localRepository = new ParseLocalRepository();
         camareroRepository = new ParseCamareroRepository();
         cocinaRepository = new ParseCocinaRepository();
         extraRepository = new ParseExtraRepository();
 
+        //iniciar Facebook
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+        // Hay que cargar la api de facebook antes de ejecutar el setContextView para que importe el boton estilo facebook
+        setContentView(R.layout.activity_main);
+
+        callbackManager = CallbackManager.Factory.create();
+        info = (TextView) findViewById(R.id.textViewFacebookInfo);
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                userId = loginResult.getAccessToken().getUserId();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Inicio de sesion cancelado.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Inicio de sesion fallido");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void buttonAddDataOnClick(View view) {
@@ -44,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ViewDataActivity.class);
         startActivity(intent);
     }
-
 
     public static CamareroRepository getCamareroRepository() {
         return camareroRepository;
@@ -61,4 +104,6 @@ public class MainActivity extends AppCompatActivity {
     public static ExtraRepository getExtraRepository() {
         return extraRepository;
     }
+
+
 }
